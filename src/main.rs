b8bccd0635a593b73d4fe2e3d629b0a016dd40d1
Raj sahani <Rajsahaniofficial19@gmail.com>
2025-02-11 09:23:46 +0530

    completed the guessing game and learned thing like match loop and use of other rust lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number between 1 and 100");
+
+    let random_number: u32 = rand::rng().random_range(1..100);
+
+    println!("please input your guess.");
+    loop {
+
+        let mut guess = String::new();
+
+        println!("random number: {}", random_number);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Too small! try again"),
+            Ordering::Greater => println!("Too big! try again"),
+            Ordering::Equal => {
+                println!("You won!");
+                break;
+            }
+        }
+
+    }
 }
